2504d3c1c37b213ad36e5da4199188c52cfa9cd2
Ryan Souza <rss15@cin.ufpe.br>
2026-05-25 17:09:12 -0300

    Remoção de funções set proportion que não vão ser utilizadas por não interferir no código e porque não vamos adaptar a interface

--- Constantes.h
+++ Constantes.h
@@ -17,17 +17,6 @@ namespace Constantes {
     constexpr std::pair<int, int> ABSTRACT_MINEFIELD_SIZE = {10, 10};
     constexpr float ABSTRACT_MINEFIELD_DENSITY = 0.12f;
     constexpr int TIME_LIMIT_SECONDS = 60;
-
-    // A implementação inline resolve o erro de "undefined reference"
-    inline std::pair<float, float> set_proportion(float a, float b) {
-        return {a, b}; 
-    }
-
-    const std::pair<float, float> MINEFIELD_SIZE = set_proportion(16.0f / 9.0f, 1.0f);
-    const std::pair<float, float> MINEFIELD_POSITION = set_proportion(4.5714f, 10000.0f);
-    const std::pair<float, float> SIZE_FLAGS = set_proportion(9.6f, 5.4f);
-    const std::pair<float, float> SIZE_BOMBS = set_proportion(9.6f, 5.4f);
-
 }
 
 #endif
